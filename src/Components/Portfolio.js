a8c1944c305e73f68616878e5b3232c6e688b885
Myouuu <meriem.mrabent@hetic.net>
2021-10-27 16:39:18 +0200

    setting up the interface:construction:

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap">
+            <Zmage alt={projects.title} src={projectImage} />
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Statistiques of the last game on Dimo</h1>
+
+
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
